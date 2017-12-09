9778acd0c67c0114879def41ff762038fa37cc57
kkiyama117 <kkiyama117@gmail.com>
2017-12-09 21:31:49 +0900

    Add settings for python
    Zoniisanのためにpythonでも走るようにしたぞ！

--- bot_test.sh
+++ bot_test.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 curl -X POST https://slack.com/api/chat.postMessage \
 -d "token=xoxb-284160486947-uuxgxY9yCrNOyvhr4nwUuS0x" \
--d "channel=#test-slackbot" \
+-d "channel=#癒やし" \
 -d "text=$1" \
 -d "as_user=true"
---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-284160486947-uuxgxY9yCrNOyvhr4nwUuS0x"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
